484a851d1ec3f0badecb361436969b632dcd94e6
debien <christopherdebien@gmail.com>
2020-07-22 11:19:42 +0200

    mise en place de tous les index

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+
+
+const app = express();
+
+app.use(express.static('public'))
+
+//route
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.get("/contact", (req, res) => {
+    res.render("contact")
+})
+
+
+
+
+
+
+
+app.listen(3000 , function() {
+    console.log("la page tourne sur le port 3000");
+})
